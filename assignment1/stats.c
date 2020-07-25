c95df4aff4db64ca451ce6805529add07eb5a749
GabrielProdi <gabriel.f.prodi@gmail.com>
2020-07-25 15:33:13 -0300

    Update stats
    Updated main and implemented some functions and variables:
        - find_median
        - find_mean
        - find_maximum
        - find_minimum
        - print_statistics

--- assignment1/stats.c
+++ assignment1/stats.c
@@ -20,73 +20,120 @@
 
 #define LENGTH 40  // Length of array
 
+unsigned char flag_sorted = 0;      // flag to ensure ordering of the database; 0 == false , 1 == true
+unsigned char maximum;      // global values for printing purposes
+unsigned char minimum;
+unsigned char mean;
+unsigned char median;
+
 int main () {
-  unsigned char array [LENGTH] = { 34, 201, 190, 154,   8, 194,   2,   6,
+  unsigned char array [ LENGTH ] = { 34, 201, 190, 154,   8, 194,   2,   6,
                                   114, 88,   45,  76, 123,  87,  25,  23,
                                   200, 122, 150, 90,   92,  87, 177, 244,
                                   201,   6,  12,  60,   8,   2,   5,  67,
                                   7,  87, 250, 230,  99,   3, 100,  90};
+  flag_sorted = 0;      // when a database is set, the data isn't sorted
+  
+  //sort_array ( array, LENGTH );      // calling the function in the main makes debugging easier 
+  
+  // filling variables
+  median = find_median ( array, LENGTH );
+  mean = find_mean ( array, LENGTH );
+  minimum = find_minimum ( array, LENGTH );
+  maximum = find_maximum ( array, LENGTH );
+  print_array ( array, LENGTH );      
 
-  sort_array (array, LENGTH);
-/*
-  unsigned char median = find_median (array, LENGTH);
-  unsigned char mean = find_mean (array, LENGTH);
-  unsigned char minimun = find_minimum (array, LENGTH);
-  unsigned char maximun = find_maximum (array, LENGTH);
-*/  
-  print_array (array, LENGTH);
-  //print_statistics();
+  print_statistics();
 
   return 0;
 }
 
 // ===================================================================================================
-//                                            FUNCTIONS
+//                                            FUNCTIONS DEFINITION
 // ===================================================================================================
  
 void print_statistics () {
-  
-
+  printf ( "In the database:\n" );
+  printf ( "Maximum: %d\n" , maximum );
+  printf ( "Minimum: %d\n" , minimum );
+  printf ( "Mean: %d\n" , mean );
+  printf ( "Median: %d\n" , median );
 }
 
-void print_array (unsigned char *data, unsigned int n) {
+void print_array ( unsigned char *data, unsigned int n ) {
   unsigned int i;
 
-  for ( i = 0; i < (n - 1); i++) {
-    printf ("%d ", data [i]);
+  for ( i = 0 ; i < ( n - 1 ) ; i++ ) {
+    printf  ("%d ", data [ i ] );
   }
-  printf ("%d \n", data [i]);
+  printf ( "%d \n", data [ i ] );
 }
 
-/*
-unsigned char find_median (unsigned char *data, unsigned int n) {
+unsigned char find_median ( unsigned char *data, unsigned int n ) {
+  unsigned int median;
+
+  if ( n <= 0 ) {
+    n = 1;
+  }
+
+  if ( ! flag_sorted ) {      // for finding the median, the dataset must be sorted
+    sort_array ( data, n );
+    flag_sorted = 1;
+  }
   
+  if ( n % 2 ) {      // if the length is odd, median is the central index
+    median = *( data + ( n / 2 ) );
+  } else {      // if the length is even, median is the average between the central indices
+    median = ( ( ( *( data + ( n / 2 ) - 1 ) ) + ( *( data + ( n / 2 ) ) ) ) / 2 );
+  }
+
+  return median;
 }
 
-unsigned char find_mean (unsigned char *data, unsigned int n) {
-  
+unsigned char find_mean ( unsigned char *data, unsigned int n ) {
+  unsigned int i, sum;
+
+  if ( n <= 0 ) {
+    n = 1;
+  }
+  for ( i = 0 ; i < n ; i++ ) {
+    sum += *( data + i );
+  }
+
+  return ( sum / n );
 }
 
-unsigned char find_maximum (unsigned char *data, unsigned int n) {
-  
+unsigned char find_maximum ( unsigned char *data, unsigned int n ) {
+  if ( ! flag_sorted ) {      // if the dataset is sorted, maximum value stands at position 0
+    sort_array ( data, n );
+    flag_sorted = 1;
+  }
+
+  return data [ 0 ];
 }
 
-unsigned char find_minimum (unsigned char *data, unsigned int n) {
-  
-}
-*/
+unsigned char find_minimum ( unsigned char *data, unsigned int n ) {
+  if ( ! flag_sorted ) {      // if the dataset is sorted, minimum value stands at last position
+    sort_array ( data, n );
+    flag_sorted = 1;
+  }
 
-void sort_array (unsigned char *data, unsigned int n){
+  return data [ n - 1 ];
+}
+
+
+void sort_array ( unsigned char *data, unsigned int n ){
   unsigned int i, j;
   
-  for (i = 0; i < (n - 1) ; i++) {
-    for (j = i + 1; j < n ; j++ ){
-      if ( *(data + i) < *(data + j)){
-        unsigned char aux = *(data + i);
-        *(data + i) = *(data + j);
-        *(data + j) = aux;
+  for ( i = 0 ; i < ( n - 1 ) ; i++ ) {
+    for ( j = i + 1 ; j < n ; j++ ){
+      if ( *( data + i ) < *( data + j ) ){
+        unsigned char aux = *( data + i );
+        *( data + i ) = *( data + j );
+        *( data + j ) = aux;
       }
     }
   }
 
+  flag_sorted = 1;
 }
--- assignment1/stats.h
+++ assignment1/stats.h
@@ -33,67 +33,80 @@ void print_statistics();
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  */
-void print_array(unsigned char *data, unsigned int n);
+void print_array( unsigned char *data, unsigned int n );
 
 /**
  * Function: find_median
  * Description: 
  *     Given an array of data and a length, returns the median value.
+ *     For finding the median, the dataset must be sorted.
+ *     If the length is odd, median is the central index. 
+ *     If the length is even, median is the average between 
+ *     the central indices
  * 
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  * 
  * @return unsigned char median : char result for median of array values
  */
- unsigned char find_median(unsigned char *data, unsigned int n);
+ unsigned char find_median( unsigned char *data, unsigned int n );
 
 /**
  * Function: find_mean
  * Description: 
- *     Given an array of data and a length, returns the mean.
+ *     Given an array of data and a length, returns the mean. 
+ *     
+ * 
  * 
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  * 
  * @return unsigned char mean : char result for mean of array values
  */
-unsigned char find_mean(unsigned char *data, unsigned int n);
+unsigned char find_mean( unsigned char *data, unsigned int n );
 
 /**
  * Function: find_maximum
  * Description: 
  *     Given an array of data and a length, returns the maximum.
+ *     If the dataset is sorted, minimum value stands at last position
  * 
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  * 
  * @return unsigned char maximum : char result for maximum of array values
  */
-unsigned char find_maximum(unsigned char *data, unsigned int n);
+unsigned char find_maximum( unsigned char *data, unsigned int n );
 
 /**
  * Function: find_minimum
  * Description: 
  *     Given an array of data and a length, returns the minimum.
+ *     If the dataset is sorted, maximum value stands at position 0
  * 
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  * 
  * @return unsigned char minimum : char result for minimum of array value
  */
-unsigned char find_minimum(unsigned char *data, unsigned int n);
+unsigned char find_minimum( unsigned char *data, unsigned int n );
 
 /**
  * Function: sort_array
  * Description: 
- *     Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ *     Given an array of data and a length, sorts the array from largest to smallest. (The zeroth
+ *     Element should be the largest value, and the last element (n-1) should be the smallest value.)
+ * 
+ *     To sort the data, each index is compared with the others. If the primary index is higher than the
+ *     other, it is placed first in the ranking. Once all the indices are compared, at the end of the
+ *     process, the data will be ordered from highest to lowest.
  * 
  * @param unsigned char *data : pointer to an n-element data array
  * @param unsigned int n : integer as the size of the array
  * 
  * @return unsigned char array: array of char result from the sort data array
  */
-void sort_array (unsigned char *data, unsigned int n);
+void sort_array ( unsigned char *data, unsigned int n );
 
 
 #endif // __STATS_H__ 
